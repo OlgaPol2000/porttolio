14094baeab9459fba0bc22de4280a9373e270e48
OlgaPol2000 <olgapolischukinstagram@gmail.com>
2022-11-06 10:43:39 +0200

    js

---
+++ index.js
@@ -0,0 +1,8 @@
+const menuBtn = document.quwerySelector(".menu")
+const navList= document.quwerySelector(".nav__list")
+
+console.log(navlist)
+
+menuBtn.addEventListener("click", () =>{
+    navlist.classList.toggle("active")
+})
